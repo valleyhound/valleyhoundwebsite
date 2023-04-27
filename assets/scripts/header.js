6bcda990f53e55b16e88c6da9c1d91320cae28d0
ValleyHound <valleyhound@valleyhound.games>
2023-04-27 14:50:51 +0100

    Bug fixes

--- assets/scripts/header.js
+++ assets/scripts/header.js
@@ -1,7 +1,7 @@
 var headerCode = `
     <img src="/assets/images/banner.png">
     <p style="text-align: center;">
-    <a href="/index.html/">Home</a><span class="prevent_select" style="color:white;">________</span><a href="/blog/">Blog</a><!--<span class="prevent_select" style="color:white;">________</span><a href="/wyomingwinter/">Wyoming Winter</a>-->
+    <a href="https://valleyhound.games">Home</a><span class="prevent_select" style="color:white;">________</span><a href="/blog/">Blog</a><!--<span class="prevent_select" style="color:white;">________</span><a href="/wyomingwinter/">Wyoming Winter</a>-->
     </p>
 
     <br>
