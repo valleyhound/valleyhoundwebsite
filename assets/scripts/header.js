3a6a813983f32ede9e6532d63d194e7d0b8d3492
ValleyHound <valleyhound@valleyhound.games>
2023-04-27 14:24:49 +0100

    Updated text on main page & created header import.

---
+++ assets/scripts/header.js
@@ -0,0 +1,12 @@
+var headerCode = `
+    <img src="assets/images/banner.png">
+    <p style="text-align: center;">
+        <u class="prevent_select">Home</u><span class="prevent_select" style="color:white;">________</span><a href="blog/">Blog</a><!--<span class="prevent_select" style="color:white;">________</span><a href="wyomingwinter/">Wyoming Winter</a>-->
+    </p>
+
+    <br>
+    <br>
+    <br>
+`;
+
+document.write(headerCode);
