f50a3875954e3e60a08848383f91d6dff63a9942
ValleyHound <valleyhound@valleyhound.games>
2024-02-24 16:13:28 +0000

    Redesign

--- assets/scripts/basicheader.js
+++ assets/scripts/basicheader.js
@@ -1,4 +1,7 @@
 var headerCode = `
+    <div style="vertical-align:middle;text-align:center;">
+        <img src="/assets/images/valleyhound hand drawn logo.png" style="width: 6rem; margin-bottom:-2rem">
+    </div>
     <div style="vertical-align:right;text-align:right;">
     <a href="https://www.youtube.com/@valleyhound" target="_blank"><img src="/assets/images/youtube_icon_white.png" style="width:32px;display: inline-block;"></a>
     <a href="https://twitter.com/ValleyHoundUK" target="_blank"><img src="/assets/images/twitter_icon_white.png" style="width:32px;display: inline-block;"></a>
@@ -12,8 +15,8 @@ var headerCode = `
     <span class="prevent_select" style="color:black;">____</span>
     <a href="/blog/">Blog</a>
 
-    <span class="prevent_select" style="color:black;">____</span>
-    <a href="/aboutus/">About Us</a>
+    <!--<span class="prevent_select" style="color:black;">____</span>-->
+    <!--<a href="/aboutus/">About</a>-->
      
     </p>
 `;
