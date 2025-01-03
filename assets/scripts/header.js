812c344f7c9bfb200d60aea8b84092663e8f77da
ValleyHound <117586830+valleyhound@users.noreply.github.com>
2025-01-03 00:53:39 +0000

    Made background full page spread image of the duo

--- assets/scripts/header.js
+++ assets/scripts/header.js
@@ -1,7 +1,5 @@
 var headerCode = `
     <script src="/assets/scripts/basicheader.js"></script>
-    <img src="/assets/images/extended_header.png" style="margin-top:2%;margin-bottom:-25%" class="prevent_select" id="headerImage">
-    <script>document.getElementById('headerImage').ondragstart = function() { return false; };</script>
     <br>
     <br>
     <br>
